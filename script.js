380e5b1852e4d66ec5fa0a053469d958462167c6
Lucasbelich <lucasbelich1@gmail.com>
2022-02-15 19:24:25 -0300

    object/array

--- script.js
+++ script.js
@@ -7,21 +7,44 @@
 let cartView = ``
 let cost = 0
 
+const juego1 = {
+    nombre: `Grand Theft Auto 5`,
+    precio: 5000
+}
+const juego2 = {
+    nombre: `FIFA 22`,
+    precio: 9000
+}
+const juego3 = {
+    nombre: `Call of Duty MW`,
+    precio: 3000
+}
+const juego4 = {
+    nombre: `Forza Horizon 5`,
+    precio: 10000
+}
+const juego5 = {
+    nombre: `Battlefield 2042`,
+    precio: 11000
+}
+
+const juegos = [juego1, juego2, juego3, juego4, juego5]
+
 const welcome = () => {
     let name = prompt(`Ingrese su nombre`)
     alert(`Bienvenido/a ${name}`)
     
 }
 
-const userSelector = () => {
+const userGameSelector = () => {
     let gameNum
     do {
     gameNum = parseInt(prompt(`Ingrese el número correspondiente al juego a comprar\n 1) Grand Theft Auto 5\n 2) FIFA 22\n 3) Call of Duty MW\n 4) Forza Horizon 5\n 5) Battlefield 2042`))
     } while ((gameNum < 1) || (gameNum > 5) || isNaN(gameNum)) 
 
-    let gameSelected
+    let gameSelected = juegos[gameNum - 1]
 
-    switch (gameNum) {
+    /* switch (gameNum) 
         case 1:
             gameSelected = `Grand Theft Auto 5`
             break
@@ -37,14 +60,14 @@ const userSelector = () => {
         case 5:
             gameSelected = `Battlefield 2042`
             break
-    }
+    } */
 
     return gameSelected
 }
 
 
 
-const priceCalc = (gameSelected) => {
+/* const priceCalc = (gameSelected) => {
    /*  if (gameSelected === `Grand Theft Auto 5`) 
             return 5000
         else if (gameSelected === `FIFA 22`)
@@ -54,7 +77,7 @@ const priceCalc = (gameSelected) => {
         else if (gameSelected === `Forza Horizon 5`)
             return 10000
         else
-            return 11000 */
+            return 11000 
     switch (gameSelected){
         case `Grand Theft Auto 5`:
             return 5000
@@ -70,33 +93,34 @@ const priceCalc = (gameSelected) => {
         default:
             return 11000
 
-    }
+    } 
+    
+} */
+
+//Desestructuracion de objeto
+const cart = ({nombre, precio}) => {
+    cartView += alert(`Juego: ${nombre}\nvalor: $${precio}`)
+    cost += precio
+
 }
 
 
-const cart = (gameSelected, gamePrice) => {
-    cartView += alert(`Juego: ${gameSelected}\nvalor: $${gamePrice}`)
-    cost += gamePrice
-
-}
-
-const payout = () => {
-    let pay = parseInt(prompt(`El monto a pagar es de $${gamePrice}\nIngrese con cuanto realizará el pago`))
+const payout = (gameSelected) => {
+    let pay = parseInt(prompt(`El monto a pagar es de $${gameSelected.precio}\nIngrese con cuanto realizará el pago`))
     if (isNaN(pay)) {
         alert(`Ingresó un caracter no valido`)
-        payout()
+        payout(gameSelected)
     } else if (pay > cost) {
         alert(`Su vuelto es $${pay - cost}\nGracias por su compra!`)
     } else if (pay === cost) {
         alert(`Gracias por su compra!`)
     } else {
         alert(`No le alcanza!`)
-        payout()
+        payout(gameSelected)
     }
 }
 
 welcome()
-let gameSelected = userSelector()
-let gamePrice = priceCalc(gameSelected)
-cart(gameSelected, gamePrice)
-payout()
+let gameSelected = userGameSelector()
+cart(gameSelected)
+payout(gameSelected)
